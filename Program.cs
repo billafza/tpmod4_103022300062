cf9dfff1cd5bdbb86e6301edc5dec37cae6a443a
billafza <nashbillanurfazza@gmail.com>
2025-03-09 19:10:46 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmod4_103022300062;
+
+class Program {
+
+    static void Main() {
+        String input = Console.ReadLine();
+
+        Enum.TryParse(input, true, out KodePos.Kelurahan kelurahan);
+        {
+            int kodePos1 = KodePos.GetKodePos(kelurahan);
+            Console.WriteLine($"Kode Pos {kelurahan}: {kodePos1}");
+        }
+    }
+}
